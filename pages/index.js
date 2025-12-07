32f912f02bceb2248b60185255a0f4a1310d7dfa
Scavazin <scavazindev@gmail.com>
2025-12-07 11:35:14 -0300

    new phrase

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 function Home() {
 
-    return <h1>❤️ Eu amo minha BUNITÍNHA ❤️</h1>
+    return <h1>❤️ Eu amo minha BUNITÍNHA❤️ 
+        Se você me ama da uma risadinha!
+    </h1>
 }
 
 export default Home;
