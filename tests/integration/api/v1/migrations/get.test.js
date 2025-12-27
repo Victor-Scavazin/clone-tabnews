ec409898e4adc7bccdff0753add04b00fe05860c
Scavazin <scavazindev@gmail.com>
2025-12-27 00:11:17 -0300

    test: rename all descriptions to use new pattern

--- tests/integration/api/v1/migrations/get.test.js
+++ tests/integration/api/v1/migrations/get.test.js
@@ -6,11 +6,15 @@ beforeAll(async () => {
   await database.query("DROP SCHEMA public CASCADE; CREATE SCHEMA public;");
 });
 
-test("GET to /api/v1/migrations should return 200", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/migrations");
-  expect(response.status).toBe(200);
+describe("GET /api/v1/status", () => {
+  describe("Anonymous user", () => {
+    test("Retrieving current system status", async () => {
+      const response = await fetch("http://localhost:3000/api/v1/migrations");
+      expect(response.status).toBe(200);
 
-  const respBody = await response.json();
-  expect(Array.isArray(respBody)).toBe(true);
-  expect(respBody.length).toBeGreaterThan(0);
+      const respBody = await response.json();
+      expect(Array.isArray(respBody)).toBe(true);
+      expect(respBody.length).toBeGreaterThan(0);
+    });
+  });
 });
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -4,25 +4,31 @@ beforeAll(async () => {
   await orchestratror.waitForAllServices();
 });
 
-test("GET to /api/v1/status should return 200", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/status");
-  expect(response.status).toBe(200);
+describe("GET /api/v1/migrations", () => {
+  describe("Anonymous user", () => {
+    describe("Retrieving pending migrations", () => {
+      test("GET to /api/v1/status should return 200", async () => {
+        const response = await fetch("http://localhost:3000/api/v1/status");
+        expect(response.status).toBe(200);
 
-  const respBody = await response.json();
-  const updatedAt = respBody.updatedAt;
-  const database = respBody.dependecies.database;
-  const maxConnections = database.maxConnections;
-  const activeConnections = database.activeConnections;
-  const postgresVersion = database.postgresVersion;
+        const respBody = await response.json();
+        const updatedAt = respBody.updatedAt;
+        const database = respBody.dependecies.database;
+        const maxConnections = database.maxConnections;
+        const activeConnections = database.activeConnections;
+        const postgresVersion = database.postgresVersion;
 
-  expect(updatedAt).toBeDefined();
-  expect(maxConnections).toBeDefined();
-  expect(activeConnections).toBeDefined();
-  expect(postgresVersion).toBeDefined();
+        expect(updatedAt).toBeDefined();
+        expect(maxConnections).toBeDefined();
+        expect(activeConnections).toBeDefined();
+        expect(postgresVersion).toBeDefined();
 
-  const parsedUpdatedAt = new Date(updatedAt).toISOString();
-  expect(updatedAt).toEqual(parsedUpdatedAt);
-  expect(maxConnections).toEqual(100);
-  expect(activeConnections).toEqual(1);
-  expect(postgresVersion).toEqual("16.0");
+        const parsedUpdatedAt = new Date(updatedAt).toISOString();
+        expect(updatedAt).toEqual(parsedUpdatedAt);
+        expect(maxConnections).toEqual(100);
+        expect(activeConnections).toEqual(1);
+        expect(postgresVersion).toEqual("16.0");
+      });
+    });
+  });
 });
